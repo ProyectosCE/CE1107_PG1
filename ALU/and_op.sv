63f137ba933b414f89d15ac830563873c5275e03
JBB092 <jos.barquero@estudiantec.cr>
2025-05-11 16:26:19 -0600

    [ADD] Implementación de las operaciones en HDL
    Se realizó la implementación de las operaciones solicitadas en el proyecto, en base al diseño realizado, tomando las consideraciones de utilizar solo compuertas y no usar estructuras de alto nivel y el operador ternario

---
+++ ALU/and_op.sv
@@ -0,0 +1,14 @@
+// ----------------------
+// Módulo AND (4 bits A, 2 bits B con replicación)
+// ----------------------
+
+module and_op(
+    input  logic [3:0] A,
+    input  logic [1:0] B,
+    output logic [3:0] Y
+);
+    assign Y[3] = A[3] & B[1];
+    assign Y[2] = A[2] & B[1];
+    assign Y[1] = A[1] & B[0];
+    assign Y[0] = A[0] & B[0];
+endmodule
