930e28c3cf567a1882c58503c5d46734affdb554
Alexander Montero V <alexmv223@pm.me>
2025-05-18 18:52:07 -0600

    [UPDATE] TestBench configuration

--- ALU/tb_alu.sv
+++ ALU/tb_alu.sv
@@ -13,7 +13,7 @@ module alu_tb;
     // Entradas
     logic [3:0] A;
     logic [1:0] B;
-    logic [1:0] sel;
+    logic [1:0] S;
 
     // Salidas
     logic [3:0] Y;
@@ -23,7 +23,7 @@ module alu_tb;
     alu dut(
         .A(A),
         .B(B),
-        .sel(sel),
+        .sel(S),
         .Y(Y),
         .Z(Z),
         .N(N),
@@ -35,25 +35,24 @@ module alu_tb;
     initial begin
 
         // Prueba 1: A=0101, B=01 (MUL, SUB, AND, XOR)
-        A = 4'b0101; B = 2'b01;
-        sel = 2'b00; #20;  // MUL
-        sel = 2'b01; #20;  // SUB
-        sel = 2'b10; #20;  // AND
-        sel = 2'b11; #20;  // XOR
+        A = 4'b0101; B = 2'b01; S = 2'b00; #20;  // MUL
+        S = 2'b01; #20;  // SUB
+        S = 2'b10; #20;  // AND
+        S = 2'b11; #20;  // XOR
 
         // Prueba 2: A=1111, B=11 (desbordamiento posible)
         A = 4'b1111; B = 2'b11;
-        sel = 2'b00; #20;
-        sel = 2'b01; #20;
-        sel = 2'b10; #20;
-        sel = 2'b11; #20;
+        S = 2'b00; #20;
+        S = 2'b01; #20;
+        S = 2'b10; #20;
+        S = 2'b11; #20;
 
         // Prueba 3: A=0000, B=00 (cero)
         A = 4'b0000; B = 2'b00;
-        sel = 2'b00; #20;
-        sel = 2'b01; #20;
-        sel = 2'b10; #20;
-        sel = 2'b11; #20;
+        S = 2'b00; #20;
+        S = 2'b01; #20;
+        S = 2'b10; #20;
+        S = 2'b11; #20;
 
     end
 
